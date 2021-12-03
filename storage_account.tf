55052411dcc0830efaec9ea72e796b6a4b2f279a
Jason2021Liu <58853510@qq.com>
2021-12-03 17:16:54 +0800

    change storage account name

--- storage_account.tf
+++ storage_account.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_storage_account" "default" {
-  name                      = local.storage_account_name
+  name                      = "storage_account_daimler_dev"
   resource_group_name       = azurerm_resource_group.rg.name
   location                  = var.resource_group_location
   account_tier             = "Standard"
