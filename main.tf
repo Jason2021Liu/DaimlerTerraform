a5e01697d54686962a3603b4fd2d418f5bde3013
Jason2021Liu <58853510@qq.com>
2021-12-03 13:08:39 +0800

    modify resource type and install name

--- main.tf
+++ main.tf
@@ -14,12 +14,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_daimler" "rg-name" {
+resource "random_pet" "rg-name" {
   prefix    = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
-  name      = random_daimler.rg-name.id
+  name      = random_pet.rg-name.id
   location  = var.resource_group_location
 }
 
