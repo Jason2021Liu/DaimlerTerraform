4c62e1d9c3f44c5254eeac0be24ef7d15d8c534a
Jason2021Liu <58853510@qq.com>
2021-12-06 15:09:49 +0800

    add tsstate in tf

--- main.tf
+++ main.tf
@@ -8,6 +8,14 @@ terraform {
       version = "~>2.0"
     }
   }
+
+  backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstate7000"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+
 }
 
 provider "azurerm" {
